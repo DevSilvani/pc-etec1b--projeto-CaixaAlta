82622f2e746f1568c99e2ede6d9c85d9645b077b
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-11 19:06:13 -0300

    C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pc_etec1b__projeto_CaixaAlta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            Console.Write("Digite uma frase: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine(nome.ToUpper());
+            Console.ReadKey();
+        }
+    }
+}
